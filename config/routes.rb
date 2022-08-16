52a2a90476ed8b9124eb0c0cec7b1fb9ff562bb4
fabiosimoesds <fabio.simoesds@gmail.com>
2022-08-16 20:14:43 +0100

    Creating the 7 actions on the Article app from scratch, using all the knowledge on the Lecture

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'articles', to: 'articles#index', as: :articles
+  post 'articles', to: 'articles#create'
+  get 'articles/new', to: 'articles#new', as: :new_article
+  get 'articles/:id', to: 'articles#show', as: :article
+  patch 'articles/:id', to: 'articles#update'
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  delete 'articles/:id', to: 'articles#destroy'
 end
